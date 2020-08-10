273252818409d304c5a1f087d02abb09f6a1d05f
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-10 12:25:01 -0400

    Make initialState an array

--- todo/src/reducers/reducer.js
+++ todo/src/reducers/reducer.js
@@ -1,6 +1,6 @@
 import Todo from "../class/Todo";
 
-export const initialState = new Todo ("Learn about reducers");
+export const initialState = [new Todo ("Learn about reducers")];
 
 export function todoReducer (state) {
   return state;
