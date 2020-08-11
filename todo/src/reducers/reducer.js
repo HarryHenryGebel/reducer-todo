0d30ae7de516d434065d533d68d70e2f18da949f
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-10 23:05:43 -0400

    Replace DELETE action with CLEAR

--- todo/src/reducers/reducer.js
+++ todo/src/reducers/reducer.js
@@ -4,10 +4,10 @@ export const initialState = [];
 
 export function todoReducer (state, action) {
   switch(action.type.toUpperCase()) {
+  case "CLEAR":
+    return state.filter((item) => !item.completed);
   case "CREATE":
     return [...state, new Todo(action.payload)];
-  case "DELETE":
-    return state.filter((item) => item.id !== action.payload);
   case "TOGGLE":
     return state.map((item) => item.id !== action.payload ?
                      item :
