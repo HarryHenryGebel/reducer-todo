fa9296a9ee2986451e309979ead7b00669714bb5
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-11 00:24:50 -0400

    Create TodoItem

---
+++ todo/src/components/TodoItem.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+
+export default function Todo(props) {
+  const {todoData, toggleCompleted} = props,
+        // change color when completed
+        color = todoData.completed ?
+        "secondary" :
+        "primary";
+
+  return (
+    <ListItem button onClick={() => toggleCompleted()}>
+          <ListItemText primary={todoData.task}
+                        primaryTypographyProps={{color: color}}/>
+        </ListItem>
+  );
+}
+
+//  LocalWords:  ListItem ListItemText
