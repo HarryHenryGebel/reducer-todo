67b5c4ef6b46632f421799bdacdde3f034098aa1
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-11 00:02:54 -0400

    Add TodoForm

---
+++ todo/src/components/TodoForm.jsx
@@ -0,0 +1,37 @@
+import React, {useState} from "react";
+import Card from "@material-ui/core/Card";
+import TextField from "@material-ui/core/TextField";
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+
+export default function TodoForm (props) {
+  const {todoList, todoDispatcher} = props,
+        [todoText, setTodoText] = useState("");
+
+  return (
+    <Card variant="outlined">
+      <h2>Add Todo Item:</h2>
+      <Box m={1}>
+        <TextField fullWidth={true}
+                   id="todo-item-field"
+                   onChange={(event) => setTodoText(event.target.value)}
+                   value={todoText}
+                   variant="filled"
+                   label="Todo Item"/>
+      </Box>
+      <br/>
+      <Box m={1} component="span">
+        <Button variant="contained"
+                disabled={this.state.todoText === ""}
+                startIcon={<AddCircleIcon/>}
+                onClick={() => {
+                  todoDispatcher({type: "ADD", payload: todoText});
+                  setTodoText("");
+                }}>
+          Add Todo Item
+        </Button>
+      </Box>
+    </Card>
+  );
+}
