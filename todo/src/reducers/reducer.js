94427ac3242f81a7e27602a617c061fe0dae1515
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-10 22:49:14 -0400

    Remove default task. Add actions.

--- todo/src/reducers/reducer.js
+++ todo/src/reducers/reducer.js
@@ -1,7 +1,18 @@
 import Todo from "../class/Todo";
 
-export const initialState = [new Todo ("Learn about reducers")];
+export const initialState = [];
 
-export function todoReducer (state) {
-  return state;
+export function todoReducer (state, action) {
+  switch(action.type) {
+  case "create":
+    return [...state, new Todo(action.payload)];
+  case "delete":
+    return state.filter((item) => item.id !== action.payload);
+  case "toggle":
+    return state.map((item) => item.id !== action.payload ?
+                     item :
+                     {...item, completed: !item.completed});
+  default:
+    throw Error(`Unknown action type "${action.type}"`);
+  }
 }
