3e18342ffc5fa701048851eaeda2a51207ad246c
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-10 23:26:28 -0400

    Use todoReducer

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, {useReducer} from "react";
 import Card from '@material-ui/core/Card';
 import "fontsource-roboto";
 
+import {initialState, todoReducer} from "./reducers/todoReducer.js";
 import "TodoList" from "./components/TodoList";
 
 function App() {
+  const [todoList, todoDispatcher] = useReducer(initialState, todoReducer);
+
   return (
     <Card variant="outlined">
-      <TodoList/>
+      <TodoList list={todoList}, dispatcher={todoDispatcher}/>
     </Card>
   );
 }
