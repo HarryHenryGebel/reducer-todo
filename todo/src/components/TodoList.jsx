929dd0b59119ca47ca2e50953c98e95a44c9ab02
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-11 00:19:15 -0400

    Create TodoList

---
+++ todo/src/components/TodoList.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
+import List from '@material-ui/core/List';
+
+import TodoItem from "./TodoItem";
+
+export default function TodoList (props) {
+  const {todoList, todoDispatcher} = props;
+
+  return (
+    <>
+      <List>
+        {todoList.map(item => (
+          <TodoItem key={item.id}
+                    item={item}
+                    toggleCompleted={() => todoDispatcher(
+                      {type: "TOGGLE", payload: item.id})}/>
+        ))}
+      </List>
+      <Box m={1} component="span">
+        <Button variant="contained"
+                onClick={todoDispatcher({type: "CLEAR"})}
+                startIcon={<HighlightOffIcon/>}>
+          Clear completed items
+        </Button>
+      </Box>
+    </>
+  );
+}
+
+//  LocalWords:  HighlightOff TodoItem
